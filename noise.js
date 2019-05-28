e537854e2c3aaff1a2bc7e88e23a55eb27fead01
Nara <naraa_520@yahoo.com>
2019-05-28 15:27:17 -0700

    Adds on click and keydown functions

--- noise.js
+++ noise.js
@@ -1,3 +1,20 @@
 $(document).ready( function() {
-  // your code here
+  $('.instrument').on('click', 'button', function(event) {
+    if ($('#'+`${$(this).html()}`+'Audio')[0].paused){
+      $('#'+`${$(this).html()}`+'Audio')[0].play();
+    } else {
+      $('#'+`${$(this).html()}`+'Audio')[0].currentTime = 0;
+    }
+    
+  });
+
+  $('body').on('keydown', function(event) {
+    if (['c','d','e','f','g','a','b'].includes(event.key)) {
+      if ($('#'+ event.key +'Audio')[0].paused){
+        $('#'+ event.key +'Audio')[0].play();  
+      } else {
+        $('#'+ event.key +'Audio')[0].currentTime = 0;
+      }  
+    } 
+  });
 });
